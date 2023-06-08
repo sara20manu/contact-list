61a6f8dece560bff77d7cc6d21fff584e3ccd812
Sara Manuela <saritamanuela20@hotmail.com>
2023-06-08 11:22:07 -0500

    mejore el codigo con eliminacion/modificacion  por nombrecontact

--- listadecontactos.js
+++ listadecontactos.js
@@ -4,69 +4,77 @@ let listaContactos = [
     nombres: "Raúl Daniel",
     apellidos: "Jímenez Rendón",
     teléfono: 311385930,
-    ubicaciones: " ",
-    ciudad: "El Carmen de Viboral",
-    dirección: "Calle principal 34 A # 140 E 45",
+    ubicaciones: {
+      ciudad: "El Carmen de Viboral",
+      dirección: "Calle principal 34 A # 140 E 45",
+    },
+  }),
+  (contacto2 = {
+    id: 19803063918,
+    nombres: "Marisol",
+    apellidos: "Mendoza Florez",
+    teléfono: 3105869930,
+    ubicaciones: {
+      ciudad: "Medellín",
+      dirección: "Carrera 5 C # 10 - 15",
+    },
+  }),
+  (contacto3 = {
+    id: 10406491230,
+    nombres: "Hemerson Camilo",
+    apellidos: "Luna Restrepo",
+    teléfono: 310750920,
+    ubicaciones: {
+      ciudad: "Itagüi",
+      dirección: "Calle 12  # 50 B 35",
+    },
   }),
 ];
 
-function añadirContacto(
-  id,
-  nombres,
-  apellidos,
-  teléfono,
-  ubicaciones,
-  ciudad,
-  dirección
-) {
-  return { id, nombres, apellidos, teléfono, ubicaciones, ciudad, dirección };
+function añadirContacto(id, nombres, apellidos, teléfono, ubicaciones = {}) {
+  return { id, nombres, apellidos, teléfono, ubicaciones };
 }
-function eliminarContacto(pos, cantidadContact) {
-  listaContactos.splice(pos, cantidadContact);
+function eliminarContacto(nombre) {
+  for (let i = 0; i < listaContactos.length; i++) {
+    if (listaContactos[i].nombres === nombre) {
+      listaContactos.splice(i, 1);
+    }
+  }
 }
 function verLista() {
   console.log(listaContactos);
 }
-function modificarContacto(posmodi, cantimodContact, modiContact) {
-  listaContactos.splice(posmodi, cantimodContact, modiContact);
+function modificarContacto(modiNombre, modiContact) {
+  for (let i = 0; i < listaContactos.length; i++) {
+    if (listaContactos[i].nombres === modiNombre) {
+      listaContactos.splice(i, 1, modiContact);
+    }
+  }
 }
 
 listaContactos.push(
-  añadirContacto(
-    23436578563,
-    "Mauricio",
-    "Espitia Lopera",
-    3224923984,
-    " ",
-    "La Ceja",
-    "Calle 2 B #03-89"
-  )
+  añadirContacto(23436578563, "Mauricio", "Espitia Lopera", 3224923984, {
+    ciudad: "La Ceja",
+    dirección: "Calle 2 B #03-89",
+  })
 );
 listaContactos.push(
-  añadirContacto(
-    19849273512,
-    "Saray Iris",
-    "González",
-    3214590607,
-    " ",
-    "El Retiro",
-    "Carrera 85  #33 A 102"
-  )
+  añadirContacto(19849273512, "Saray Iris", "González", 3214590607, {
+    ciudad: "El Retiro",
+    dirección: "Carrera 85  #33 A 102",
+  })
 );
-
+verLista();
 modificarContacto(
-  0,
-  1,
+  "Raúl Daniel",
   añadirContacto(
     13267204693,
     "Lina María",
     "Montemayor Sépulveda",
     3129734068,
-    " ",
-    "El Carme de Viboral",
-    "Diagonal 45 C #43 - 102"
+    { ciudad: "El Carme de Viboral", dirección: "Diagonal 45 C #43 - 102" }
   )
 );
 
-eliminarContacto(1, 1);
+eliminarContacto("Mauricio");
 verLista();
